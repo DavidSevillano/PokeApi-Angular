2358bda74fe6f619ad0ebf6576b68331299e243b
Burixer85 <dvd.sevillano@gmail.com>
2024-10-23 21:09:53 +0200

    Movimientos en proceso

--- PokeApi/src/app/components/moves-list/moves-list.component.ts
+++ PokeApi/src/app/components/moves-list/moves-list.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Move } from '../../models/moves.interface';
+import { MoveService } from '../../services/move.service';
+
 
 @Component({
   selector: 'app-moves-list',
   templateUrl: './moves-list.component.html',
   styleUrl: './moves-list.component.css'
 })
-export class MovesListComponent {
+
+export class MovesListComponent implements OnInit {
+
+  moveList: Move[] = [];
+
+  constructor(private moveService: MoveService) {}
+
+  ngOnInit(): void {
+    this.moveService.getMoveList().subscribe(resp => {
+      this.moveList = resp.results;
+    });
+  }
+
+  getmoveId(url: string): number {
+    var id = url.split('/')[6];
+    return parseInt(id);
+  }
+
+  
 
 }
+
+
+
+
